418a81c8c6270cb108d50537869c17fd570e0b56
Nicholas Boll <nicholas.boll@gmail.com>
2015-08-09 14:21:36 -0600

    Updating paths

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ shell.rm('-rf', 'build');
 shell.exec('git clone https://github.com/NicholasBoll/blog.git build');
 shell.exec('cd build; git checkout gh-pages');
 shell.rm('-rf', ['build/articles', 'build/tag', 'build/css'])
-shell.exec('wintersmith build');
-shell.exec('cd build; git add .');
+shell.exec('wintersmith build --baseUrl="/blog"');
+shell.exec('cd build; git add -A');
 shell.exec('cd build; git commit -m "Auto-deploy"');
 shell.exec('cd build; git push origin gh-pages -f');
